66ff4d1312853e41e76540460517d285d9073a72
Philippe Fanaro <philippefanaro@gmail.com>
2021-02-09 23:18:32 -0300

    merge

---
+++ Real-World Haskell/6/eqclasses.hs
@@ -0,0 +1,40 @@
+-- Typeclasses look like mixins as interfaces
+-- or abstract mixins?
+class BasicEq a where
+  isEqual :: a -> a -> Bool
+
+-- isEqual :: (BasicEq a) => a -> a -> Bool
+
+instance BasicEq Bool where
+  isEqual True True = True
+  isEqual False False = True
+  isEqual _ _ = False
+
+--------------------------------------------------------------------------------
+
+class BasicEq2 a where
+  isEqual2 :: a -> a -> Bool
+  isNotEqual2 :: a -> a -> Bool
+
+--------------------------------------------------------------------------------
+
+-- People implementing this class must provide an implementation of at least
+-- one function. They can implement both if they wish, but they will not be
+-- required to
+class BasicEq3 a where
+  isEqual3 :: a -> a -> Bool
+  isEqual3 x y = not (isNotEqual3 x y)
+
+  isNotEqual3 :: a -> a -> Bool
+  isNotEqual3 x y = not (isEqual3 x y)
+
+--------------------------------------------------------------------------------
+
+main = do
+  putStrLn "Please enter a Double:"
+  inpStr <- getLine
+  let inpDouble = read inpStr :: Double
+  putStrLn ("Twice " ++ show inpDouble ++ " is " ++ show (inpDouble * 2))
+
+--------------------------------------------------------------------------------
+
